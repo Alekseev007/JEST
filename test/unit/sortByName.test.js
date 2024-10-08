8b02f15bda349930cea260e2e0fcf10864cc509b
Vasiliy Alekseev <alekseev.v007@yandex.ru>
2024-10-08 14:25:44 +0300

    Hometask

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,26 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
+describe("Books names test suit2", () => {
+  it("No sort", () => {
+    expect(sorting.sortByName(["Гарри Поттер", "Гарри Поттер"])).toEqual([
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]);
+  });
+});
